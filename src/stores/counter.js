1001013717bf317225f380e0678653979ebffa4d
issam ahwach <issamahwach00@gmail.com>
2022-04-22 20:31:36 +0300

    General Updates

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 
+//example of pinia state management (not used in this project):
 export const useCounterStore = defineStore({
   id: "counter",
   state: () => ({
